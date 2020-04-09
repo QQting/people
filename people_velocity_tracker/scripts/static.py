770144de2c242c670ccd737de142f2e587bcff40
Ting Chang <qting0529@hotmail.com>
2020-04-09 17:23:59 +0800

    Support new cmdline argument for targeting frame_id.

--- people_velocity_tracker/scripts/static.py
+++ people_velocity_tracker/scripts/static.py
@@ -11,17 +11,25 @@ class VelocityTracker(object):
         self.ppub = rospy.Publisher('/people', People, queue_size=10)
 
     def spin(self):
+        if len(sys.argv) != 6:
+            print("Usage: ./static.py frame_id pos_x pos_y vel_x vel_y\n");
+            print("For example: ./static.py map 2.0 1.0 3.0 3.0\n")
+            sys.exit()
+
+
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             pv = Person()
             pl = People()
             pl.header.stamp = rospy.Time.now()
-            pl.header.frame_id = 'base_link'
-            pv.position.x = float(sys.argv[1])
-            pv.position.y = float(sys.argv[2])
+
+            # determine which frame's coordinator you rely on, usually set to map or base_link
+            pl.header.frame_id = sys.argv[1]
+            pv.position.x = float(sys.argv[2])
+            pv.position.y = float(sys.argv[3])
             pv.position.z = .5
-            pv.velocity.x = float(sys.argv[3])
-            pv.velocity.y = float(sys.argv[4])
+            pv.velocity.x = float(sys.argv[4])
+            pv.velocity.y = float(sys.argv[5])
             pv.name = 'asdf'
             pv.reliability = .90
             pl.people.append(pv)
